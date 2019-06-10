765fb360dd43c18c0fddff6af87406704345781e
GeorgeMujuru <gmujuru@touchsides.com>
2019-06-10 20:52:05 +0200

    second

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.toasterlibrary.ToasterMessage
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ToasterMessage.s(this,"Hello World")
     }
 }
